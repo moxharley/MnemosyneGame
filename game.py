1d14c45b0bbe54a391067a5f39cd6d238026b3ad
Harlan Bullock <harlanbull7@gmail.com>
2025-11-07 14:12:15 -0800

    Adds output(message).

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
-print('Popal was late today.')
+import time
+import sys
+
+def output(message):
+    delay = 0.25
+    for char in message:
+        sys.stdout.write(char)
+        time.sleep(delay)
+
+def character():
+    pass
+
+def game():
+    pass
+
+def main():
+    output('> Test Message')
+
+if __name__ == "__main__":
+    main()
