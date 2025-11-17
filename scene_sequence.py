d58ca6ecdfba7bee48c09a07789675b97e935e84
Harlan Bullock <harlanbull7@gmail.com>
2025-11-17 14:57:16 -0800

    Fixes starting current-HP.

--- scene_sequence.py
+++ scene_sequence.py
@@ -193,7 +193,7 @@ def boot():
     last_name = input()
     identifier1 = first_name
     identifier2 = 'Ensign ' + last_name
-    player = {'X-coordinate': 1, 'Y-coordinate': 1, 'first-name': first_name, 'last-name': last_name, 'current-HP': 10, 'current-EP': 10, 'room': 'Stasis-Pods'}
+    player = {'X-coordinate': 1, 'Y-coordinate': 1, 'first-name': first_name, 'last-name': last_name, 'current-HP': 7, 'current-EP': 10, 'room': 'Stasis-Pods'}
 
     output('> ', True)
     archangel_output(f'{first_name} {last_name}...acknowledged. Welcome back {identifier1}.')
