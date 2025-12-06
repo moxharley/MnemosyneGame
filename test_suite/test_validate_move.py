18c0ba5fe80c620c7576553eadda0f31e28a4316
Harlan Bullock <harlanbull7@gmail.com>
2025-12-05 16:56:26 -0800

    Adds test_validate_move.

--- test_suite/test_validate_move.py
+++ test_suite/test_validate_move.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
+from logic import validate_move
 
 
 class Test(TestCase):
-    def test_validate_move(self):
-        self.fail()
+    def test_valid_move(self):
+        example_board = {(0, 0): "Empty Room", (0, 1): "Empty Room"}
+        example_character = {"X-coordinate": 0, "Y-coordinate": 0}
+        example_movement = 'n'
+        self.assertTrue(validate_move(example_board, example_character, example_movement))
+
+    def test_invalid_move(self):
+        example_board = {(0, 0): "Empty Room", (0, 1): "Empty Room"}
+        example_character = {"X-coordinate": 0, "Y-coordinate": 0}
+        example_movement = 'e'
+        self.assertFalse(validate_move(example_board, example_character, example_movement))
