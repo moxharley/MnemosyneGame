4613bbb1797171216aee540befe75417ba36639c
Harlan Bullock <harlanbull7@gmail.com>
2025-12-05 16:34:50 -0800

    Adds more unit tests.

--- test_suite/test_level_up.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class Test(TestCase):
-    def test_level_up(self):
-        self.fail()
--- test_suite/test_roll.py
+++ test_suite/test_roll.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+from unittest.mock import patch
+from logic import roll
 
 
 class Test(TestCase):
-    def test_roll(self):
-        self.fail()
+    @patch('random.randint', return_value=6)
+    def test_roll_success(self, _):
+        expected = True
+        actual = roll(0, 3)
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_failure(self, _):
+        expected = False
+        actual = roll(0, 3)
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=3)
+    def test_roll_bound(self, _):
+        expected = True
+        actual = roll(0, 3)
+        self.assertEqual(expected, actual)
