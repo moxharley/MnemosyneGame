87fc050493c11ecd4cbd1b9a554daa376421e919
Harlan Bullock <harlanbull7@gmail.com>
2025-12-05 16:14:04 -0800

    Adds energy_drain unit tests.

--- test_suite/test_energy_drain.py
+++ test_suite/test_energy_drain.py
@@ -1,6 +1,30 @@
 from unittest import TestCase
+from logic import energy_drain
+from unittest.mock import patch
+import pygame
 
 
 class Test(TestCase):
-    def test_energy_drain(self):
-        self.fail()
+    @patch('random.random', return_value=1)
+    def test_no_drain(self, _):
+        example_character = {'current-EP': 10}
+        expected = {'current-EP': 10}
+        actual = energy_drain(example_character)
+        self.assertEqual(expected, actual)
+
+    @patch('random.random', return_value=0)
+    def test_drain(self, _):
+        pygame.mixer.pre_init(frequency=44100, size=-16, channels=4, buffer=512)
+        pygame.mixer.init()
+        pygame.mixer.set_num_channels(4)
+        example_character = {'current-EP': 10}
+        expected = {'current-EP': 9}
+        actual = energy_drain(example_character)
+        self.assertEqual(expected, actual)
+
+    @patch('random.random', return_value=0.20)
+    def test_bound(self, _):
+        example_character = {'current-EP': 10}
+        expected = {'current-EP': 10}
+        actual = energy_drain(example_character)
+        self.assertEqual(expected, actual)
